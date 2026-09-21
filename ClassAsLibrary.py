6287efd52033350b427ea75bb6fc39a11a2c7b2b
Jagadeesan <Jagadesh043@gmail.com>
2026-09-21 10:50:17 +0530

    Created the new Class_Functions class for importing the functions inside the ClassAsLibrary

--- ClassAsLibrary.py
+++ ClassAsLibrary.py
@@ -1,8 +1,27 @@
 # from classname import functionname
 from Class import Calculator;
+from Class_Functions import FuncAssignment;
 
 BMIIndex = Calculator.BMI();
 print(BMIIndex);
 
 CheckOddOEven = Calculator.OddorEven(23);
 print(CheckOddOEven);
+
+SubFields = FuncAssignment.SubFields();
+
+FuncAssignment.OddEven();
+
+FuncAssignment.ElegiblityForMarriage();
+
+FuncAssignment.Percentage();
+
+FuncAssignment.Triangle();
+
+
+
+
+
+
+
+
---
+++ Class_Functions.py
@@ -0,0 +1,47 @@
+class FuncAssignment:
+
+    #1.SubFields
+    def SubFields():
+        Fields = ['Machine Learning','Neural Networks','Vision','Robotics','Speech Processing','Natural Language Processing'];
+        print("Sub-Fields in AI are:")
+        for Field in Fields:
+            print(Field);
+
+    #2.OddEven  
+    def OddEven():
+        number = int(input("Enter your Number:"));
+        if(number%2==0):
+            print(number ,"is Even Number");
+        else:
+            print(number ,"is Odd Number");
+
+    #3.ElegiblityForMarriage
+    def ElegiblityForMarriage():
+        Age = int(input("Your Age:"));
+        if(Age<=20):
+            print("Not Eligible");
+        else:
+            print("Eligible");
+
+    #4 Percentage.
+    def Percentage():
+        lstPercentage = [98,87,95,95,93];
+        i=0;
+        total=0;
+        for percent in lstPercentage:
+            i+=1;
+            print("Subject",i,"=",percent);
+            total+=percent;
+        print("Total:",total);
+        print("Percentage: ",total/len(lstPercentage));
+
+    #5 Triangle.
+    def Triangle():
+        height = int(input("Height:"));
+        breadth = int(input("Breadth:"));
+        print("Area of Triangle:",height*breadth/2);
+        height1 = int(input("Height1:"));
+        height2 = int(input("Height2:"));
+        breadth1 = int(input("Breadth:"));
+        print("Perimeter Triangle:",height1+height2+breadth1);
+
